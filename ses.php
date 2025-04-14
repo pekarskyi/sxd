d9dd63438afb6bff64ff80b01282feefe9a49e21
Mykola Pekarskyi <dizer7@gmail.com>
2025-04-15 01:25:24 +0300

    Add files via upload

---
+++ ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  'b536101234cc6ba8e5c633ffe6e911a7' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'uk',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'root',
+      'my_pass' => '',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '7',
+      'exitURL' => './',
+    ),
+    'time' => 1744624740,
+    'lng' => 'uk',
+  ),
+);
+?>
